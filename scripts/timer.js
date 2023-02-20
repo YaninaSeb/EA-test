d47b0ceb24710a9f922b0ebc3a37322317ccf2c8
YaninaSeb <sebrukovich_jana@mail.ru>
2023-02-20 17:06:50 +0300

    feat: add timer script

---
+++ scripts/timer.js
@@ -0,0 +1,38 @@
+const days = document.querySelector('.timer__days');
+const hours = document.querySelector('.timer__hours');
+const minutes = document.querySelector('.timer__minutes');
+const seconds = document.querySelector('.timer__seconds');
+
+let milisecInSec = 1000;
+let milisecInMin = 60 * milisecInSec;
+let milisecInHour = 60 * milisecInMin;
+let milisecInDays = 24 * milisecInHour;
+
+let deadline = new Date(2023, 4, 31).getTime();
+
+document.addEventListener('DOMContentLoaded', () => {
+    let timerId = setInterval(getTime, 1000);
+});
+
+function getTime() {
+    let currentTime = new Date();
+    let diff = deadline - currentTime;
+
+    if (diff <= 0) clearInterval(timerId);
+
+    let countDays = Math.floor(diff / milisecInDays);
+    diff = diff - (countDays * milisecInDays);
+
+    let countHours = Math.floor(diff / milisecInHour);
+    diff = diff - (countHours * milisecInHour);
+
+    let countMin = Math.floor(diff / milisecInMin);
+    diff = diff - (countMin * milisecInMin);
+
+    let countSec = Math.floor(diff / milisecInSec);
+
+    days.textContent = countDays;
+    hours.textContent = countHours;
+    minutes.textContent = countMin;
+    seconds.textContent = countSec;
+}
